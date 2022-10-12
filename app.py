e21f9ab0e47ef20ae3ef2dfc9ca8ef845f2c3dfa
bushycargo <josephaines03@gmail.com>
2022-10-12 18:48:03 +0100

    Bug Fix

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html");
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():  # put application's code here
-    return render_template("about.html");
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
