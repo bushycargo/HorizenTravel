37a8adfc46b54b1fa7bd8fd5a77fa95b38475e53
bushycargo <bushycargo@github.com>
2023-04-03 11:16:33 +0100

    Fixed a number of bugs and got SQL connection working successfully.

--- dbfunc.py
+++ dbfunc.py
@@ -1,44 +1,54 @@
 import json
 
-import pandas
-import pymysql
-from sshtunnel import SSHTunnelForwarder
+import mysql.connector
 
 
 class DatabaseHandler:
     def __init__(self):
         self.dbconnection = None
         self.config = getConfig()
-        self.tunnel = self.makeTunnel()
-
-    def makeTunnel(self):
-        tunnel = SSHTunnelForwarder(
-            (self.config['ssh_host'], 22),
-            ssh_username=self.config['ssh_username'],
-            ssh_pkey=self.config['ssh_pkey'],
-            remote_bind_address=(self.config['localhost'], 3306)
-        )
-        tunnel.start()
-        print("Tunnel Connection Successful")
-        tunnel.close()
-        return tunnel
 
     def connect(self):
-        self.tunnel.start()
-        self.dbconnection = pymysql.connect(
+        # self.tunnel.start() # Depreciated, Not used anymore since server migration to Scaleway
+        self.dbconnection = mysql.connector.connect(
             host=self.config['localhost'],
+            port=self.config['port'],
             user=self.config['database_username'],
-            passwd=self.config['database_password'],
-            db=self.config['database_name'],
-            port=self.tunnel.local_bind_port
+            password=self.config['database_password'],
+            database=self.config['database_name']
         )
+        if self.dbconnection.is_connected():
+            return
+        else:
+            print("Issue during DB Connection")
+            exit(0)
 
     def runSQL(self, sql):
-        return pandas.read_sql_query(sql, self.dbconnection)
+        if self.dbconnection.is_connected():
+            cursor = self.dbconnection.cursor()
+            cursor.execute(sql)
+            output = cursor.fetchall()
+            self.dbconnection.commit()
+            cursor.close()
+            return output
 
     def disconnect(self):
-        self.tunnel.close()
+        # self.tunnel.close()
         self.dbconnection.close()
+
+    def getFlightData(self):
+        with open('flightData.json', 'r') as file:
+            flightdata = json.load(file)
+        print(flightdata)
+
+        for flight in flightdata:
+            print(flight)
+            origin = flight['origin']
+            destination = flight['destination']
+            departure = flight['departure_time']
+            arrival = flight['arrival_time']
+            bookings = 120
+            self.runSQL(f"INSERT INTO `jh-horizen-travel`.flight (origin, destination, depart_time, arrive_time, bookings) VALUES ('{origin}', '{destination}', '{departure}', '{arrival}', '{bookings}');")
 
 
 def getConfig():
