0b3d0ff02ec2cfc3eec448542c883ef88d455261
bushycargo <josephaines03@gmail.com>
2023-04-05 23:38:55 +0100

    HTML - Basic prep for HTML files for login system.

---
+++ static/js/sign-out.js
@@ -0,0 +1,11 @@
+function signOut(){
+    setCookie("auth_token", "", 0)
+    window.location.replace("/login")
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
